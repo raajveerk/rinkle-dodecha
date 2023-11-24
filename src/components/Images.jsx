b7019ec1b245fb0ddceedbc50f7767a1a351fd71
raajveerk <khattarraajveer@gmail.com>
2023-11-24 17:02:08 +0530

    actually updated the correct white kurti photo (dos --> dos_new)

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import uno from "../images/uno.png";
-import dos from "../images/dos.png";
+import dos_new from "../images/dos_new.png";
 import tres from "../images/tres.png";
 import quatro from "../images/quatro.png";
 import sinko from "../images/sinko.png";
@@ -22,7 +22,7 @@ export const Images = () => {
     </div>
     <div className='mb-[150px] max-w-[1000px] lg:flex hidden lg:mx-auto space-x-[50px]'>
         <div>
-            <img src={dos} alt="Dos" className='w-[300px] h-[300px] rounded-2xl'/>
+            <img src={dos_new} alt="Dos" className='w-[300px] h-[300px] rounded-2xl'/>
         </div>
         <div>
             <img src={quatro} alt="Quatro" className='w-[300px] h-[300px] bg-fixed rounded-2xl' />
@@ -49,7 +49,7 @@ export const Images = () => {
             <img src={ses} alt="Ses" className='w-[500px] h-[500px] bg-fixed rounded-2xl' />
         </div><br />
         <div>
-            <img src={dos} alt="Dos" className='w-[500px] h-[500px] bg-fixed rounded-2xl'/>
+            <img src={dos_new} alt="Dos" className='w-[500px] h-[500px] bg-fixed rounded-2xl'/>
         </div><br />
     </div>
     <div className='sm:hidden flex flex-col items-center space-y-[25px] mb-[0px]'>
@@ -69,7 +69,7 @@ export const Images = () => {
             <img src={ses} alt="Ses" className='w-[300px] h-[300px] bg-fixed rounded-2xl' />
         </div><br />
         <div>
-            <img src={dos} alt="Dos" className='w-[300px] h-[300px] bg-fixed rounded-2xl'/>
+            <img src={dos_new} alt="Dos" className='w-[300px] h-[300px] bg-fixed rounded-2xl'/>
         </div><br />
     </div>
     </>
